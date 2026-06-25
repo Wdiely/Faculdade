1039c33a90b1210167b6c93d674bd0db44df8075
Wdiely Martins <161127502+Wdiely@users.noreply.github.com>
2026-06-25 20:36:05 -0300

    feito

--- lista 05 (struct)/ex09 fazer.cpp
+++ lista 05 (struct)/ex09 fazer.cpp
@@ -2,30 +2,31 @@
 #include <cmath>
 using namespace std;
 
-struct tri {
-    double a;
-    double b;
-    double c;
-    
-    double calcularDistancia(tri& p1) {
-	    double distanciaX = p1.x - p2.x;
-		double distanciaY = p1.y - p2.y;
-	    return sqrt(distanciaX * distanciaX + distanciaY * distanciaY);
-	}
+struct Triangulo {
+    float xA, yA;
+    float xB, yB;
+    float xC, yC;
+
+    float area() {
+        return fabs((xA * (yB - yC) +
+                     xB * (yC - yA) +
+                     xC * (yA - yB)) / 2.0);
+    }
 };
 
-
 int main() {
-    tri p1;
-    cout << "coordenadas ponto a: ";
-    cin >> p1.a;
-    cout << "coordenadas ponto b: ";
-    cin >> p1.b;
-	cout << "coordenadas ponto c: ";
-    cin >> p1.c;
-    
-    double distancia = calcularDistancia(p1);
-    cout << "a distancia entre os pontos e: " << distancia << endl;
+    Triangulo t;
+
+    cout << "Digite as coordenadas do ponto A (x y): ";
+    cin >> t.xA >> t.yA;
+
+    cout << "Digite as coordenadas do ponto B (x y): ";
+    cin >> t.xB >> t.yB;
+
+    cout << "Digite as coordenadas do ponto C (x y): ";
+    cin >> t.xC >> t.yC;
+
+    cout << "Area = " << t.area() << endl;
 
     return 0;
 }
